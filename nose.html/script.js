493bb4dcdb47930024a8421f5365c003b3ed957d
Garu006 <garojas@uamv.edu.ni>
2026-04-28 14:25:04 -0600

    mejoramiento de formato y ajustes en archivo .js

--- nose.html/script.js
+++ nose.html/script.js
@@ -1,15 +1,16 @@
+console.log("Javascript cargado correctamente");
 //Animación al hacer scroll
-const sectioon = document.querySelectorAll("Section");
+const sections = document.querySelectorAll("section");
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            entry.target.classList.add("visible");        
+            entry.target.classList.add("visible");
         }
     });
 });
 
-section.forEach(section => {
+sections.forEach(section => {
     observer.observe(section);
     section.classList.add("hidden");
 });
@@ -17,20 +18,19 @@ section.forEach(section => {
 // navbar dinamico
 const nav = document.querySelector("nav");
 
-window.addEventListener("Scroll", () => {
+window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
         nav.style.backgroundColor = "#d6cbb7";
     } else {
         nav.style.backgroundColor = "transparent";
     }
 });
-
 // modo oscuro
-const themeButton = document.getElementById("theme-toogle");
+const themeButton = document.getElementById("theme-toggle");
 
 themeButton.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
-    
+
     if (document.body.classList.contains("dark-mode")) {
         themeButton.textContent = "Modo Claro";
 
